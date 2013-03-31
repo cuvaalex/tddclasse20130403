b50717c55d1c684dd6717a444ba3a695f23ec480
cuvaalex <alexandre.cuva@gmail.com>
2013-03-31 15:42:39 +0200

    Add TestSonar

---
+++ TestSonar/SimpleCase/Calculateur.cs
@@ -0,0 +1,34 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Calculateur.cs" company="Zenika">
+//   dd
+// </copyright>
+// <summary>
+//   The calculator.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Com.Zenika.TDD {
+    using System.Diagnostics.CodeAnalysis;
+
+    /// <summary>
+    /// The calculator.
+    /// </summary>
+    public class Calculateur {
+        /// <summary>
+        /// The add.
+        /// </summary>
+        /// <param name="valeur1">
+        /// The valeur 1.
+        /// </param>
+        /// <param name="valeur2">
+        /// The valeur 2.
+        /// </param>
+        /// <returns>
+        /// The <see cref="int"/>.
+        /// </returns>
+        public int Add(int valeur1, int valeur2)
+        {
+            return 9;
+        }
+    }
+}
---
+++ TestSonar/SimpleTest/CalculateurTest.cs
@@ -0,0 +1,28 @@
+﻿namespace Com.Zenika.TDD {
+    using FluentAssertions;
+
+    using NUnit.Framework;
+
+    /// <summary>
+    /// The add test.
+    /// </summary>
+    [TestFixture]
+    public class CalculateurTest {
+        /// <summary>
+        /// The quand adition 5 et 4 alors egal 9.
+        /// </summary>
+        [Test]
+        public void QuandAdition5Et4AlorsEgal9()
+        {
+            var valeur1 = 5;
+            var valeur2 = 4;
+            var expected = 9;
+
+            var calculateur = new Calculateur();
+            var resultat = calculateur.Add(valeur1, valeur2);
+
+            resultat.Should().BePositive("Ajout de 2 chiffre positif donne un chiffre positif").And.Be(expected);
+        }
+    }
+
+}
